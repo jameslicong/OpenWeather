df2b33b8ff833ff193d4ca0780b15283e731cad6
James Erickson Licong <james.licong@upraxis.com>
2020-07-09 23:37:33 +0800

    initial project setup

---
+++ app/src/main/java/com/smilefactory/openweather/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.smilefactory.openweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smilefactory.openweather.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
