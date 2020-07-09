783b50d8b9962ed870935ac433bc6afcf62c34a5
James Erickson Licong <james.licong@upraxis.com>
2020-07-10 00:46:36 +0800

    Add RxModule

--- app/src/main/java/com/smilefactory/openweather/utilities/UtilitiesModule.kt
+++ app/src/main/java/com/smilefactory/openweather/utilities/UtilitiesModule.kt
@@ -1,11 +1,13 @@
 package com.smilefactory.openweather.utilities
 
 import com.smilefactory.openweather.utilities.framework.FrameworkModule
+import com.smilefactory.openweather.utilities.rx.RxModule
 import dagger.Module
 
 @Module(
     includes = [
-        FrameworkModule::class
+        FrameworkModule::class,
+        RxModule::class
     ]
 )
 abstract class UtilitiesModule {
