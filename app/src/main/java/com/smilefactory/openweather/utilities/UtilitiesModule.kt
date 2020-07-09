47956f415a0a0fe15b342f849d11ad9860c2b937
James Erickson Licong <james.licong@upraxis.com>
2020-07-10 00:41:58 +0800

    Include FrameworkModule in UtilitiesModule

--- app/src/main/java/com/smilefactory/openweather/utilities/UtilitiesModule.kt
+++ app/src/main/java/com/smilefactory/openweather/utilities/UtilitiesModule.kt
@@ -1,8 +1,13 @@
 package com.smilefactory.openweather.utilities
 
+import com.smilefactory.openweather.utilities.framework.FrameworkModule
 import dagger.Module
 
-@Module
+@Module(
+    includes = [
+        FrameworkModule::class
+    ]
+)
 abstract class UtilitiesModule {
 
 }
