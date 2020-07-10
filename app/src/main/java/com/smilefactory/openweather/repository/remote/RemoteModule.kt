6e871af5290ec2f5eb4c9476a731f0d0d948f2ed
James Licong <james.licong@gmail.com>
2020-07-10 23:14:44 +0800

    Remove ApiModule

--- app/src/main/java/com/smilefactory/openweather/repository/remote/RemoteModule.kt
+++ app/src/main/java/com/smilefactory/openweather/repository/remote/RemoteModule.kt
@@ -1,13 +1,13 @@
 package com.smilefactory.openweather.repository.remote
 
-import com.smilefactory.openweather.repository.remote.api.ApiModule
+import com.smilefactory.openweather.repository.remote.api.ApiFactory
+import com.smilefactory.openweather.repository.remote.api.ApiFactoryImp
 import com.smilefactory.openweather.repository.remote.loader.RemoteLoaderModule
 import dagger.Binds
 import dagger.Module
 
 @Module(
     includes = [
-        ApiModule::class,
         RemoteLoaderModule::class
     ]
 )
