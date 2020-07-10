6b171edc46ccc8d4c5ea61dccc91601bcad3679e
James Licong <james.licong@gmail.com>
2020-07-10 21:15:17 +0800

    Update MainActivity: Use data binding

--- app/src/main/java/com/smilefactory/openweather/screens/MainActivity.kt
+++ app/src/main/java/com/smilefactory/openweather/screens/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.smilefactory.openweather.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.smilefactory.openweather.R
+import com.smilefactory.openweather.databinding.MainActivityBinding
 import com.smilefactory.openweather.screens.list.MainFragment
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var binding : MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment())
+            .commitNow()
+
+
     }
 }
