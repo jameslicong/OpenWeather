401463edb1e30963746c4675220dcf75b19bbeb0
James Licong <james.licong@gmail.com>
2020-07-11 12:37:12 +0800

    Add DatabaseModule

---
+++ app/src/main/java/com/smilefactory/openweather/repository/local/database/DatabaseModule.kt
@@ -0,0 +1,23 @@
+package com.smilefactory.openweather.repository.local.database
+
+import androidx.room.Room
+import com.smilefactory.openweather.Application
+import com.smilefactory.openweather.BuildConfig
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideSqliteDatabase(application: Application): Database {
+        return Room.databaseBuilder(
+            application,
+            Database::class.java,
+            BuildConfig.DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
