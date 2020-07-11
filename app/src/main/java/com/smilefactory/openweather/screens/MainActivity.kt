e39907cdabfc5b1807c8ddfb631331385c32725f
James Licong <james.licong@gmail.com>
2020-07-11 23:01:39 +0800

    Update MainActivity: show WeatherForecastListFragment

--- app/src/main/java/com/smilefactory/openweather/screens/MainActivity.kt
+++ app/src/main/java/com/smilefactory/openweather/screens/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.smilefactory.openweather.R
 import com.smilefactory.openweather.databinding.MainActivityBinding
-import com.smilefactory.openweather.screens.list.MainFragment
+import com.smilefactory.openweather.screens.list.WeatherForecastListFragment
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -15,12 +15,8 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
-
-
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, MainFragment())
+            .replace(R.id.container, WeatherForecastListFragment())
             .commitNow()
-
-
     }
 }
