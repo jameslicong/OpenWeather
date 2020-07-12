98896f30ec61a2039ecf25d64f3b1a7884172a6c
James Licong <james.licong@gmail.com>
2020-07-12 14:18:55 +0800

    Add WeatherForecastDetailsFragment and ViewModel

--- app/src/main/java/com/smilefactory/openweather/screens/details/DetailsModule.kt
+++ app/src/main/java/com/smilefactory/openweather/screens/details/DetailsModule.kt
@@ -1,6 +1,13 @@
 package com.smilefactory.openweather.screens.details
 
+import com.smilefactory.openweather.utilities.dagger.FragmentScope
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
-abstract class DetailsModule {}
+abstract class DetailsModule {
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    abstract fun weatherForecastDetailsFragment(): WeatherForecastDetailsFragment
+}
